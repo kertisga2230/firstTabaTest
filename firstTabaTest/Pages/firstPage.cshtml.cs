cb5f80a5ac65c809d9072d3fae2cdf5eb9e72a5d
Kertis <itamar.kertis@gmail.com>
2025-02-09 23:14:25 +0200

    Add project files.

---
+++ firstTabaTest/Pages/firstPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace firstTabaTest.Pages
+{
+    public class firstPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
